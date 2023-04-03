da9e2b79480c42d6e52ec8da8bfe9e6f32f27b44
Gavin Bloom <70165980+gavin-bloom@users.noreply.github.com>
2023-04-03 15:26:23 -0400

    comments

--- server.cpp
+++ server.cpp
@@ -15,8 +15,8 @@ using namespace std;
 
 const int BUFFER_SIZE = 1024;
 
-list<string> server_messages;
-shared_mutex rw_mutex;
+list<string> server_messages; //shared message list
+shared_mutex rw_mutex; //shared read/write lock
 
 void handle_client(int client_socket) {
     cout << "Client connected. Thread ID: " << this_thread::get_id() << endl;
@@ -37,36 +37,36 @@ void handle_client(int client_socket) {
         } else {
             buffer[bytes_read] = '\0';
             cout << "Received message: " << buffer << endl;
-			string cur = buffer;
-			
-			//send list of messages back
-			if (cur == "get") {
-				string response = "";
-				for (const auto& msg : server_messages) {
-                    response += msg + "\n";
+	    string cur = buffer;
+
+	    //send list of messages back
+	    if (cur == "get") {
+		string response = "";
+		for (const auto& msg : server_messages) {
+	    	    response += msg + "\n";
                 }
-				send(client_socket, response.c_str(), response.length(), 0);
-			} else {
-            // Release the read lock before acquiring the write lock
-            read_lock.unlock();
+		send(client_socket, response.c_str(), response.length(), 0);
+	    } else {
+	        // Release the read lock before acquiring the write lock
+	        read_lock.unlock();
 
-            // Acquire the write lock to modify the shared list of messages
-            unique_lock<shared_mutex> write_lock(rw_mutex);
-          
-            server_messages.push_back(buffer);
-            cout << "Added message to list." << endl;
+	        // Acquire the write lock to modify the shared list of messages
+	        unique_lock<shared_mutex> write_lock(rw_mutex);
 
-			//simulate work
-            sleep(1);
-            // Release the write lock
-            write_lock.unlock();
-    
+	        server_messages.push_back(buffer);
+	        cout << "Added message to list." << endl;
 
-            // Acquire the read lock again to allow concurrent read access
-            read_lock.lock();
-            string response = "Message received.\n";
-            send(client_socket, response.c_str(), response.length(), 0);
-			}
+		//simulate work
+	        sleep(1);
+	        // Release the write lock
+	        write_lock.unlock();
+
+
+	        // Acquire the read lock again to allow concurrent read access
+	        read_lock.lock();
+	        string response = "Message received.\n";
+	        send(client_socket, response.c_str(), response.length(), 0);
+	    }
         }
     }
 
@@ -82,12 +82,15 @@ int main(int argc, char* argv[]) {
     }
 
     int port = atoi(argv[1]);
+	
+    // Create the server socket
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
         cerr << "Error creating socket." << endl;
         exit(EXIT_FAILURE);
     }
-
+    
+    // Bind server socket to server address
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
@@ -99,6 +102,7 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Start listnening for connections
     if (listen(server_socket, SOMAXCONN) < 0) {
         cerr << "Error listening on socket." << endl;
         exit(EXIT_FAILURE);
@@ -106,6 +110,7 @@ int main(int argc, char* argv[]) {
 
     cout << "Server listening on port " << port << endl;
 
+    // Accept client connections and handle them in separate threads
     while (true) {
         int client_socket = accept(server_socket, NULL, NULL);
         if (client_socket < 0) {
